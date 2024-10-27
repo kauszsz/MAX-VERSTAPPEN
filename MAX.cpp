b795979958ee9ae26575fbe13115062df8f456eb
kauszsz <kaua.sonia.fabio@gmail.com>
2024-10-27 00:40:24 -0300

    Add files via upload

---
+++ MAX.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+
+    std::string myString = "teste";
+
+    myString = "TU TU TU RU ";
+
+    myString += "MAX VERSTAPPEN";
+
+    std::cout << myString << "\n";
+
+    system("pause");
+
+    return 0;
+}
